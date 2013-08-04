d44b1ef5c2b26b2f66edf44fbca78de255f9d7ac
naheas <naheas@hanmail.net>
2013-08-04 16:58:18 +0900

    naheas second reupload

--- src/com/example/mtmerge/NewGroupActivity.java
+++ src/com/example/mtmerge/NewGroupActivity.java
@@ -2,7 +2,11 @@ package com.example.mtmerge;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class NewGroupActivity extends Activity {
 
@@ -10,6 +14,24 @@ public class NewGroupActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_newgroup);
+	}
+	
+	public void go_btn_newgroup_finish (View v) {
+
+		String groupnameStr = ((EditText)findViewById(R.id.et_newgroup_groupname)).getText().toString();
+		String boyStr = ((EditText)findViewById(R.id.et_newgroup_boy)).getText().toString();
+		String girlStr = ((EditText)findViewById(R.id.et_newgroup_girl)).getText().toString();
+		String placeStr = ((EditText)findViewById(R.id.et_newgroup_place)).getText().toString();
+
+		Intent intent_grouplist = new Intent();
+	
+		intent_grouplist.putExtra("groupname", groupnameStr);
+	    intent_grouplist.putExtra("boy", boyStr);
+	    intent_grouplist.putExtra("girl", girlStr);
+	    intent_grouplist.putExtra("place", placeStr);
+	    setResult(RESULT_OK, intent_grouplist);
+	    
+		finish();
 	}
 
 	@Override
--- src/com/example/mtmerge/Tab1Activity.java
+++ src/com/example/mtmerge/Tab1Activity.java
@@ -1,22 +1,413 @@
 package com.example.mtmerge;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
+
+
 import android.app.Activity;
-import android.view.Menu;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class Tab1Activity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_tab1);
-	}
+	private ListView mListView = null;
+	// Data를 관리해주는 Adapter
+	private CustomAdapter mCustomAdapter = null;
+	// 제네릭(String)을 사용한 ArrayList
+	private ArrayList<String> mArrayList = new ArrayList<String>();
+
+	int group_position;	
+	int boyNum = 0;
+	int girlNum = 0;
+
+	String group_id;
+	String memTbName;
+	
+	SQLiteDatabase db_mt;
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.tab1, menu);
-		return true;
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tab1);
+		
+		Intent myintent = getIntent();
+		group_position = myintent.getExtras().getInt("group_position");
+		
+		db_mt = openOrCreateDatabase("db_mt", MODE_PRIVATE, null);
+		
+		initBoyGirlNumTbName();
+
+		createifnotexistMemberTable();
+
+
+		initList();
+		
+		mListView = (ListView) findViewById(R.id.lv_tab1_member);
+		mCustomAdapter = new CustomAdapter(Tab1Activity.this, mArrayList);
+		mListView.setAdapter(mCustomAdapter);
+		mListView.setOnItemClickListener(new mItemClickListener());
+		mListView.setOnItemLongClickListener( new ListViewItemLongClickListener() );
+	}
+
+	protected void onDestroy() {
+		db_mt.close();
+		super.onDestroy();
+	}
+
+	// 남자 여자 수만큼 추가
+	private void initBoyGirlNumTbName() {
+		group_id = getGroupId();
+		memTbName = "tb_member_" + group_id;
+		
+		String boyStr = getDataGr(1);
+		if (isStringInt(boyStr)) {
+			boyNum = Integer.parseInt(boyStr);
+		}
+		String girlStr = getDataGr(2);
+		if (isStringInt(girlStr)) {
+			girlNum = Integer.parseInt(girlStr);
+		}		
+		
+		((TextView)findViewById(R.id.tv_tab1_allnum)).setText(String.valueOf(boyNum + girlNum));
+	}
+	
+	private void initList() {
+        String sql = "select mem_name, money from " + memTbName;
+		Cursor cursor = db_mt.rawQuery(sql, null);
+		cursor.moveToFirst();
+		for(int i = 0; i < cursor.getCount(); i++){
+			mArrayList.add(cursor.getString(0));
+			cursor.moveToNext();
+		}
+		cursor.close();
+	}
+
+	public void addMember(View v) {
+		AlertDialog alertDlg = new AlertDialog.Builder(Tab1Activity.this)
+				.create();
+		alertDlg.setTitle("Add New Member");
+		alertDlg.setMessage("새로운 맴버의 성별은?");
+
+		alertDlg.setButton(DialogInterface.BUTTON_POSITIVE, "남자",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						insertDataMem("새로운 남자 이름 ", false, false);
+						updateBoyGirlGr(false, true); // boy, up
+						mCustomAdapter.add("새로운 남자 이름 ");
+						mCustomAdapter.notifyDataSetChanged();
+						boyNum++;
+						((TextView)findViewById(R.id.tv_tab1_allnum)).setText(String.valueOf(boyNum + girlNum));
+						return;
+					}
+				});
+
+		alertDlg.setButton(DialogInterface.BUTTON_NEGATIVE, "여자",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						insertDataMem("새로운 여자 이름 ", false, true);
+						updateBoyGirlGr(true, true); // girl, up
+						mCustomAdapter.add("새로운 여자 이름 ");
+						mCustomAdapter.notifyDataSetChanged();
+						girlNum++;
+						((TextView)findViewById(R.id.tv_tab1_allnum)).setText(String.valueOf(boyNum + girlNum));
+						return;
+					}
+				});
+		
+		alertDlg.setButton(DialogInterface.BUTTON_NEGATIVE, "취소",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						return;
+					}
+				});
+
+		alertDlg.show();
+	}
+
+	// ListView 안에 Item을 클릭시에 호출되는 Listener
+	private class mItemClickListener implements OnItemClickListener {
+
+		public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+				long arg3) {
+			mCustomAdapter.setChecked(position);
+			// Data 변경시 호출 Adapter에 Data 변경 사실을 알려줘서 Update 함.
+			mCustomAdapter.notifyDataSetChanged();
+
+		}
+	}
+	
+	private class ListViewItemLongClickListener implements OnItemLongClickListener
+	   {
+	       @Override
+	       public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) 
+	       {
+	           final String selectedStr = getDataMem(position, 0);
+	           AlertDialog alertDlg = new AlertDialog.Builder(Tab1Activity.this).create();
+	           alertDlg.setTitle("Delete?");
+	           alertDlg.setMessage("Do you want to delete " + selectedStr + "?");
+	           // '예' 버튼이 클릭되면
+	           alertDlg.setButton(DialogInterface.BUTTON_POSITIVE, "Yes", new DialogInterface.OnClickListener()
+	           {
+	                @Override
+	                public void onClick( DialogInterface dialog, int which ) 
+	                {
+	                	if(getDataMem(position, 2).equalsIgnoreCase("1")) girlNum--;
+	                	else boyNum--;
+	                	((TextView)findViewById(R.id.tv_tab1_allnum)).setText(String.valueOf(boyNum + girlNum));
+	                	
+	                    deleteDataMem(position);	            		
+	            		if(getDataMem(position, 2).equalsIgnoreCase("1")) updateBoyGirlGr(true, false); // girl, down
+	            		else updateBoyGirlGr(false, false); //boy, down
+	            		
+	                	mCustomAdapter.remove(position);
+	                    mCustomAdapter.notifyDataSetChanged();       
+	                    
+	                	return;
+	                }
+	           });
+	           
+	           // '아니오' 버튼이 클릭되면
+	           alertDlg.setButton(DialogInterface.BUTTON_NEGATIVE, "No", new DialogInterface.OnClickListener()
+	           {
+	                @Override
+	                public void onClick( DialogInterface dialog, int which ) {
+	                	return;
+	                }
+	           });
+	           
+	           
+	           alertDlg.show();
+	           return true;
+	       }
+	    
+	   }
+
+	// Custom Adapter
+	class CustomAdapter extends BaseAdapter {
+
+		private ViewHolder viewHolder = null;
+		// 뷰를 새로 만들기 위한 Inflater
+		private LayoutInflater inflater = null;
+		private ArrayList<String> sArrayList = new ArrayList<String>();
+		private ArrayList<Boolean> isCheckedConfrim = new ArrayList<Boolean>();;
+
+		public CustomAdapter(Context c, ArrayList<String> mList) {
+			inflater = LayoutInflater.from(c);
+			this.sArrayList = mList;
+			// ArrayList Size 만큼의 boolean 배열을 만든다.
+			// CheckBox의 true/false를 구별 하기 위해
+			for (int i = 0; i < sArrayList.size(); i++) {
+				this.isCheckedConfrim.add(false);
+			}
+		}
+
+		public void add(String tempStr) {
+			sArrayList.add(tempStr);
+			isCheckedConfrim.add(false);
+		}
+		
+		public void remove(int position) {
+			sArrayList.remove(position);
+			isCheckedConfrim.remove(position);
+		}
+
+		public void setChecked(int position) {
+			boolean tempBool = !isCheckedConfrim.get(position);
+			isCheckedConfrim.set(position, tempBool); // isCheckedConfrim[position]
+														// = ischeked; 체크하기
+		}
+
+		public ArrayList<Integer> getChecked() {
+			int tempSize = isCheckedConfrim.size();
+			ArrayList<Integer> mArrayList = new ArrayList<Integer>();
+			for (int b = 0; b < tempSize; b++) {
+				if (isCheckedConfrim.get(b)) {
+					mArrayList.add(b);
+				}
+			}
+			return mArrayList;
+		}
+
+		@Override
+		public int getCount() {
+			return sArrayList.size();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			// ConvertView가 null 일 경우
+			View v = convertView;
+
+			if (v == null) {
+				viewHolder = new ViewHolder();
+				// View를 inflater 시켜준다.
+				v = inflater.inflate(R.layout.tab1_memberlist_row, null);
+				viewHolder.cBox = (CheckBox) v.findViewById(R.id.btn_tab1_add);
+				v.setTag(viewHolder);
+			}
+
+			else {
+				viewHolder = (ViewHolder) v.getTag();
+			}
+
+			// CheckBox는 기본적으로 이벤트를 가지고 있기 때문에 ListView의 아이템
+			// 클릭리즈너를 사용하기 위해서는 CheckBox의 이벤트를 없애 주어야 한다.
+			viewHolder.cBox.setClickable(false);
+			viewHolder.cBox.setFocusable(false);
+
+			viewHolder.cBox.setText(sArrayList.get(position));
+			// isCheckedConfrim 배열은 초기화시 모두 false로 초기화 되기때문에
+			// 기본적으로 false로 초기화 시킬 수 있다.
+			viewHolder.cBox.setChecked(isCheckedConfrim.get(position));
+
+			return v;
+		}
+	}
+
+	class ViewHolder {
+		// 새로운 Row에 들어갈 CheckBox
+		private CheckBox cBox = null;
+	}
+
+	private void createifnotexistMemberTable() { // 테이블 생성 메소드
+		// 테이블 생성 쿼리를 정의합니다. id값과 x y 를 텍스트형태로 만듭니다.
+		Cursor cursor = db_mt.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
+		cursor.moveToFirst();
+		for(;;){
+			if(cursor.getString(0).equalsIgnoreCase(memTbName)){
+				break;
+			}
+			if (!cursor.moveToNext()) {
+				String sql = "create table if not exists " + memTbName + "(mem_id INTEGER PRIMARY KEY AUTOINCREMENT, mem_name text, money integer, sex integer)";
+				db_mt.execSQL(sql);
+
+				for (int i = 0; i < girlNum; i++) {
+					insertDataMem("여자 이름 " + i, false, true);
+				}
+				for (int i = 0; i < boyNum; i++) {
+					insertDataMem("남자 이름 " + i, false, false);
+				}
+				break;
+			}
+		}
+		cursor.close();
+	}
+
+	
+
+	private void insertDataMem(String mem_name, boolean moneyBool, boolean sexBool) {
+		int moneyNum;
+		int sexNum;
+		
+		if(moneyBool) moneyNum = 1;
+		else moneyNum = 0;
+		if(sexBool) sexNum = 1; // 1=Girl
+		else sexNum = 0;// 0=Boy
+		
+		String sql = "insert into " + memTbName + "(mem_name, money) values('"
+				+ mem_name + "', '" + moneyNum + "', '" + sexNum + "')";
+		// 마찬가지로, 정의한 쿼리를 보냅니다.
+		db_mt.execSQL(sql);
+	}
+	
+	private void deleteDataMem(int position) {// 쿼리로 값 받아오는 메소드
+		// GPS라는 테이블로부터 id,x,y값을 받아오겠다고 정의합니다.
+		String sql = "select mem_id from " + memTbName;
+		// 정의한 쿼리를 보내기 전에, Cousor라는 친구에게 넣어줍니다.
+		Cursor cursor = db_mt.rawQuery(sql, null);
+		cursor.moveToPosition(position);
+		int tempn = cursor.getInt(0);
+		cursor.close();
+		db_mt.execSQL("delete from " + memTbName + " where mem_id = '" + tempn + "'");
+	}
+
+	
+	private String getDataMem(int position, int which) {// 쿼리로 값 받아오는 메소드
+		// GPS라는 테이블로부터 id,x,y값을 받아오겠다고 정의합니다.
+		String sql = "select mem_name, money, sex from " + memTbName;
+		// 정의한 쿼리를 보내기 전에, Cousor라는 친구에게 넣어줍니다.
+		Cursor cursor = db_mt.rawQuery(sql, null);
+		cursor.moveToPosition(position);
+		String temps;
+		if(which == 0) temps = cursor.getString(which);
+		else temps = String.valueOf(cursor.getInt(which));
+		cursor.close();
+		return temps;
+	}
+
+	private String getDataGr(int which) {// 쿼리로 값 받아오는 메소드
+		// GPS라는 테이블로부터 id,x,y값을 받아오겠다고 정의합니다.
+		String sql = "select groupname, boy, girl, place from tb_group";
+		// 정의한 쿼리를 보내기 전에, Cousor라는 친구에게 넣어줍니다.
+		Cursor cursor = db_mt.rawQuery(sql, null);
+		cursor.moveToPosition(group_position);
+		String temps = cursor.getString(which);
+		cursor.close();
+		return temps;
+	}
+	
+	private String getGroupId() {
+		String sql = "select group_id from tb_group";
+		Cursor cursor = db_mt.rawQuery(sql, null);
+		cursor.moveToPosition(group_position);
+		int tempn = cursor.getInt(0);
+		cursor.close();
+		String tempStr = String.valueOf(tempn);
+		return tempStr;
+	}
+	
+	private void updateBoyGirlGr(boolean boyOrGirl, boolean downOrUp){ // boy, down = false // girl, up = true
+		int tempNum;
+		if(boyOrGirl) tempNum = Integer.parseInt(getDataGr(2));
+		else tempNum = Integer.parseInt(getDataGr(1));
+		if(downOrUp) tempNum++;
+		else tempNum--;
+		
+		String tempStr;
+		if(boyOrGirl) tempStr = "girl";
+		else tempStr = "boy";
+				
+		String sql = "UPDATE tb_group SET " + tempStr + " = "+ String.valueOf(tempNum) + " WHERE group_id = " + group_id;
+		db_mt.execSQL(sql);
+	}
+
+	public static boolean isStringInt(String s) {
+		try {
+			Integer.parseInt(s);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 
 }
+
